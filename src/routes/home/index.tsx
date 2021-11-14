fdb2476c4d929793dbc0071626fb2f3a1cf661bc
gusjasponde <gusjasponde@protonmail.com>
2021-11-14 20:03:48 -0300

    feat: add proper logic to verification

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,8 +1,11 @@
 import { FunctionalComponent, h } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
 import get from 'axios';
+import { BsFillFileCheckFill, BsFillFolderFill, BsFillQuestionSquareFill } from 'react-icons/bs';
 import { isUrlValid } from '../../utils/validation';
 import style from './style.css';
+import Loading from '../../components/loading';
+import Info from '../../components/info';
 
 const Home: FunctionalComponent = function () {
   const api = `${window.location.href.replace('3000', '3001')}api/greeting`;
@@ -12,14 +15,19 @@ const Home: FunctionalComponent = function () {
   const [inputTimeout, setInputTimeout] = useState<any>();
   // used to check if url is valid and how many were tested
   const [urlCounter, setUrlCounter] = useState(0);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [linkType, setlinkType] = useState<string>();
 
-  // If url valid, check its content
+  // Effect for checking url content
   useEffect(() => {
-    const timeout = 500 + (urlCounter * 10);
+    const timeout = 500;
     const debounceTimeout = setTimeout(async () => {
       if (urlValue) {
-        const response = await get(api).then((res:any) => JSON.parse(res.data));
-        console.log(urlCounter, urlValue, `${timeout}`, response);
+        const response = await get(api, { params: { url: urlValue }})
+          .then((res:any) => JSON.parse(res.data));
+        const { type } = response;
+        setlinkType(type);
+        setLoading(false);
       }
     }, timeout);
     return () => clearTimeout(debounceTimeout);
@@ -32,13 +40,18 @@ const Home: FunctionalComponent = function () {
 
     setInputTimeout(setTimeout(() => {
       setUrlValue((prevState : string) => {
+        if (value === prevState) return prevState;
+
+        setLoading(true);
         if (isUrlValid(value)) {
           setUrlCounter(urlCounter + 1);
           return value;
         }
+        setlinkType('invalid');
+        setLoading(false);
         return prevState;
       });
-    }, 800));
+    }, 500));
   };
 
   const onSubmit = (e : h.JSX.TargetedEvent<HTMLFormElement, Event>) => {
@@ -56,7 +69,11 @@ const Home: FunctionalComponent = function () {
           onKeyUp={(e) => onKeyUp(e?.target?.value)}
           className={style.input}
         />
+        {isLoading ? (
+          <Loading />
+        ) : <Info type={linkType} />}
       </form>
+
     </div>
   );
 };
